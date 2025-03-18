032a18e5fb294903cc5501ebb747954a09744f39
Jihwan Lee <119032680+roovies@users.noreply.github.com>
2025-03-18 12:39:10 +0900

    [Bronze V] Title: A-B, Time: 104 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int num1 = Integer.parseInt(input[0]);
+        int num2 = Integer.parseInt(input[1]);
+        
+        System.out.println(num1 - num2);
+	}
+}
