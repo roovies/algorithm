92c37308bbaa64851340c4425454e858428ddbc1
Jihwan Lee <119032680+roovies@users.noreply.github.com>
2025-04-10 20:31:26 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 30.27 ms, Memory: 98.5 MB -BaekjoonHub

--- 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -2,26 +2,12 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String myString) {
-        List<String> list = new ArrayList<>();
-        String[] arr = myString.split("x");
-        for (String str : arr) {
-            if(str.equals(""))
-                continue;
-            list.add(str);
-        }
-        // x가 포함된 문자열이 주어짐
-        // x를 구분자로 잘라내 배열을 만들고, 사전순으로 정렬한 배열을 return
-        // 빈 문자열을 반환할 배열에 넣지 않음
-        // 문자열의 길이는 최대 10만
-        
-        Collections.sort(list);
-        
-        String[] answer = new String[list.size()];
-        
-        for(int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
-        }
-        
-        return answer;
+        StringTokenizer st = new StringTokenizer(myString, "x");
+        int idx = 0, N = st.countTokens();
+        String[] result = new String[N];
+        for (int n = 0;n < N;n++)
+            result[idx++] = st.nextToken();
+        Arrays.sort(result);
+        return result;
     }
 }
